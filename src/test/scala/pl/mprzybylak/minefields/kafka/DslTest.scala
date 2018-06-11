467bc40556c7aa2c1cc741d288ac347696b1d502
Michał Przybylak <michal.przybylak@gmail.com>
2018-06-11 23:17:38 +0200

    WIP

--- src/test/scala/pl/mprzybylak/minefields/kafka/DslTest.scala
+++ src/test/scala/pl/mprzybylak/minefields/kafka/DslTest.scala
@@ -6,7 +6,7 @@ import net.manub.embeddedkafka.{EmbeddedKafka, EmbeddedKafkaConfig}
 import org.scalatest.{Matchers, WordSpec}
 import net.manub.embeddedkafka.{EmbeddedKafka, EmbeddedKafkaConfig}
 import org.apache.kafka.common.serialization.{Serde, Serdes, StringSerializer}
-import org.apache.kafka.streams.{Consumed, StreamsBuilder, StreamsConfig}
+import org.apache.kafka.streams.{Consumed, KeyValue, StreamsBuilder, StreamsConfig}
 import net.manub.embeddedkafka.streams.EmbeddedKafkaStreamsAllInOne
 import org.apache.kafka.streams.kstream._
 import net.manub.embeddedkafka.Codecs.stringKeyValueCrDecoder
@@ -14,7 +14,8 @@ import net.manub.embeddedkafka.Codecs._
 import net.manub.embeddedkafka.ConsumerExtensions._
 import net.manub.embeddedkafka.EmbeddedKafkaConfig
 import org.apache.kafka.common.serialization.{Serde, Serdes}
-import org.apache.kafka.streams.{Consumed, StreamsBuilder}
+import org.apache.kafka.streams.processor.{AbstractProcessor, ProcessorContext, ProcessorSupplier, PunctuationType}
+import org.apache.kafka.streams.state.KeyValueStore
 import org.scalatest.{Matchers, WordSpec}
 
 import scala.concurrent.duration._
@@ -119,6 +120,75 @@ class DslTest extends WordSpec with EmbeddedKafkaStreamsAllInOne with Matchers {
       }
     }
 
+
+    "deduplicate with procesor api" in {
+
+      val deduplicationWindowDuration = 1 seconds
+      val deduplicationRetention = deduplicationWindowDuration.toMillis * 2 + 1
+      val deduplicationWindow = TimeWindows.of(deduplicationWindowDuration.toMillis).until(deduplicationRetention)
+
+      val streamsConfiguration = Map[String, String](
+        StreamsConfig.COMMIT_INTERVAL_MS_CONFIG -> "1000"
+      )
+
+
+      val evPvDeduplicator: Reducer[String] =
+        (first, second) => {
+          println("reducer. first: " + first + " second: " + second)
+          if(first.equals(second)) first else second
+        }
+
+      val evPvToClientKeyMapper: KeyValueMapper[Windowed[String], String, String] =
+        (windowedEvPvKey, _) => {
+          println("window mapper " + windowedEvPvKey.key )
+          windowedEvPvKey.key
+        }
+
+      val streamBuilder = new StreamsBuilder()
+
+      val stream: KStream[String, String] =
+        streamBuilder.stream(inTopic, Consumed.`with`(stringSerde, stringSerde))
+
+      val deduplicatedStream: KStream[String, String] = stream.groupByKey(Serialized.`with`(stringSerde, stringSerde))
+        .reduce(evPvDeduplicator, "reduce-store")
+        .toStream()
+
+      val procesorSup: ProcessorSupplier[String, String] = () => new DuplicateProcessor()
+        val ddStr = deduplicatedStream.process(procesorSup(), "reduce-store")
+
+      ddStr.to(outTopic, Produced.`with`(stringSerde, stringSerde))
+
+      runStreams(Seq(inTopic, outTopic), streamBuilder.build(), streamsConfiguration) {
+
+        publishToKafka(inTopic, "1", "duplicate-message1")
+        Thread.sleep(100)
+        publishToKafka(inTopic, "1", "duplicate-message2")
+        Thread.sleep(100)
+        publishToKafka(inTopic, "1", "duplicate-message2")
+
+        withConsumer[String, String, Unit] { consumer =>
+
+
+          val consumedMessages: Stream[(String, String)] =
+            consumer.consumeLazily(outTopic)
+
+
+          consumedMessages.take(3) should be(
+            Seq(
+              "1" -> "duplicate-message1",
+              "1" -> "duplicate-message2"
+            )
+          )
+        }
+      }
+    }
+
+  }
+}
+
+class DuplicateProcessor extends AbstractProcessor[String, String] {
+
+  override def process(key: String, value: String): Unit = {
 
   }
 }
