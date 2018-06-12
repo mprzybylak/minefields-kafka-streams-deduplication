4a3b52f80d158e48e3503667bcbce275e97fde76
Michał Przybylak <michal.przybylak@gmail.com>
2018-06-12 09:17:34 +0200

    deduplication with procesor api

--- src/test/scala/pl/mprzybylak/minefields/kafka/DslTest.scala
+++ src/test/scala/pl/mprzybylak/minefields/kafka/DslTest.scala
@@ -14,8 +14,10 @@ import net.manub.embeddedkafka.Codecs._
 import net.manub.embeddedkafka.ConsumerExtensions._
 import net.manub.embeddedkafka.EmbeddedKafkaConfig
 import org.apache.kafka.common.serialization.{Serde, Serdes}
-import org.apache.kafka.streams.processor.{AbstractProcessor, ProcessorContext, ProcessorSupplier, PunctuationType}
+import org.apache.kafka.streams.kstream.Materialized._
+import org.apache.kafka.streams.processor._
 import org.apache.kafka.streams.state.KeyValueStore
+import org.apache.kafka.streams.state.internals.MeteredKeyValueBytesStore
 import org.scalatest.{Matchers, WordSpec}
 
 import scala.concurrent.duration._
@@ -123,25 +125,16 @@ class DslTest extends WordSpec with EmbeddedKafkaStreamsAllInOne with Matchers {
 
     "deduplicate with procesor api" in {
 
-      val deduplicationWindowDuration = 1 seconds
-      val deduplicationRetention = deduplicationWindowDuration.toMillis * 2 + 1
-      val deduplicationWindow = TimeWindows.of(deduplicationWindowDuration.toMillis).until(deduplicationRetention)
-
       val streamsConfiguration = Map[String, String](
-        StreamsConfig.COMMIT_INTERVAL_MS_CONFIG -> "1000"
+        "cache.max.bytes.buffering" -> "0"
+//        StreamsConfig.COMMIT_INTERVAL_MS_CONFIG -> "1000"
       )
 
 
       val evPvDeduplicator: Reducer[String] =
         (first, second) => {
           println("reducer. first: " + first + " second: " + second)
-          if(first.equals(second)) first else second
-        }
-
-      val evPvToClientKeyMapper: KeyValueMapper[Windowed[String], String, String] =
-        (windowedEvPvKey, _) => {
-          println("window mapper " + windowedEvPvKey.key )
-          windowedEvPvKey.key
+          second
         }
 
       val streamBuilder = new StreamsBuilder()
@@ -153,9 +146,13 @@ class DslTest extends WordSpec with EmbeddedKafkaStreamsAllInOne with Matchers {
         .reduce(evPvDeduplicator, "reduce-store")
         .toStream()
 
-      val procesorSup: ProcessorSupplier[String, String] = () => new DuplicateProcessor()
-        val ddStr = deduplicatedStream.process(procesorSup(), "reduce-store")
+      val transformerSupplier: TransformerSupplier[String, String, KeyValue[String, String]] = () => new DuplicateTransformer()
 
+//      val procesorSup: ProcessorSupplier[String, String] = () => new DuplicateProcessor()
+//      deduplicatedStream.process(procesorSup, "reduce-store")
+//      deduplicatedStream.to(outTopic, Produced.`with`(stringSerde, stringSerde))
+
+      val ddStr = deduplicatedStream.transform(transformerSupplier, "reduce-store")
       ddStr.to(outTopic, Produced.`with`(stringSerde, stringSerde))
 
       runStreams(Seq(inTopic, outTopic), streamBuilder.build(), streamsConfiguration) {
@@ -173,7 +170,7 @@ class DslTest extends WordSpec with EmbeddedKafkaStreamsAllInOne with Matchers {
             consumer.consumeLazily(outTopic)
 
 
-          consumedMessages.take(3) should be(
+          consumedMessages.take(10) should be(
             Seq(
               "1" -> "duplicate-message1",
               "1" -> "duplicate-message2"
@@ -188,7 +185,43 @@ class DslTest extends WordSpec with EmbeddedKafkaStreamsAllInOne with Matchers {
 
 class DuplicateProcessor extends AbstractProcessor[String, String] {
 
+  private lazy val pvStore: MeteredKeyValueBytesStore[String,String] =
+    context().getStateStore("reduce-store").asInstanceOf[MeteredKeyValueBytesStore[String, String]]
+
+
   override def process(key: String, value: String): Unit = {
+    val kv = pvStore.get(key)
+    println(s"procesor. key: $key value: $value")
+    println(s"store. value: $kv")
+  }
+}
+
+class DuplicateTransformer extends Transformer[String, String, KeyValue[String, String]] {
+
+  var ctx:ProcessorContext = null
+  var stateStore: MeteredKeyValueBytesStore[String, String] = null
+
+  override def init(context: ProcessorContext): Unit = {
+    ctx = context
+      stateStore = ctx.getStateStore("reduce-store").asInstanceOf[MeteredKeyValueBytesStore[String, String]]
+  }
+
+  override def transform(key: String, value: String): KeyValue[String, String] = {
+    val oldValue = stateStore.get(key)
+    println(s"store. value: $oldValue")
+    println(s"transformer. key: $key value: $value")
+
+    if(value.equals(oldValue)) {
+      return null
+    }
+    new KeyValue[String, String](key, value)
+  }
+
+  override def punctuate(timestamp: Long): KeyValue[String, String] = {
+    return null;
+  }
+
+  override def close(): Unit = {
 
   }
 }
